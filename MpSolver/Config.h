b5c7c620536861090d31fbf1bef8ee7cc44c6c93
suzhouxing <suzhouxing@qq.com>
2019-09-19 14:14:41 +0800

    add callback.

--- MpSolver/Config.h
+++ MpSolver/Config.h
@@ -5,17 +5,37 @@
 /// note  : 1.	tag macros with [on/off] to indicate prefered state.
 ////////////////////////////////
 
-#ifndef AGAIN_SZX_MP_SOLVER_CONFIG_H
-#define AGAIN_SZX_MP_SOLVER_CONFIG_H
+#ifndef SMART_SZX_P_CENTER_CONFIG_H
+#define SMART_SZX_P_CENTER_CONFIG_H
 
 
 #pragma region PlatformCheck
 #ifdef _MSC_VER // use (_WIN16 || _WIN32 || _WIN64)?
 #define _OS_MS_WINDOWS  1
-#define _CC_MSVC  1
+#define _CC_MS_VC  1
+
+#if _MSC_VER >= 1910
+#define _CC_VERSION  2017
+#elif _MSC_VER >= 1900
+#define _CC_VERSION  2015
+#elif _MSC_VER >= 1800
+#define _CC_VERSION  2013
+#elif _MSC_VER >= 1700
+#define _CC_VERSION  2012
+#elif _MSC_VER >= 1600
+#define _CC_VERSION  2010
+#elif _MSC_VER >= 1500
+#define _CC_VERSION  2008
+#elif _MSC_VER >= 1400
+#define _CC_VERSION  2005
+#elif _MSC_VER
+#define _CC_VERSION  2003
+#else // _MSC_VER
+#define _CC_VERSION  OTHER_COMPILER // error
+#endif // _MSC_VER
 #else
 #define _OS_MS_WINDOWS  0
-#define _CC_MSVC  0
+#define _CC_MS_VC  0
 #endif // _MSC_VER
 
 #ifdef __unix__
@@ -58,29 +78,25 @@
 #pragma region LinkLibraryCheck
 #if (_DLL || _SHARED) && !(_STATIC) // prefer static when both are (un)defined.
 #define _LL_DYNAMIC  1
+#define _LL_STATIC  0
 #else
+#define _LL_DYNAMIC  0
 #define _LL_STATIC  1
 #endif // _DLL
 
 #if (_DEBUG || DEBUG) && !(NDEBUG || _NDEBUG || RELEASE || _RELEASE) // prefer release when both are (un)defined.
+#define _DR_RELEASE  0
 #define _DR_DEBUG  1
 #else
 #define _DR_RELEASE  1
+#define _DR_DEBUG  0
 #endif // _DEBUG
 #pragma endregion LinkLibraryCheck
 
 
 #pragma region DebugHelper
-// [off] activate test codes.
-#define SZX_DEBUG  1
-
-#if SZX_DEBUG
-// [off] print additional debug information. check DebugLog.h for fine-grained control.
-#define SZX_DEBUG_LOG  1
-
-// [off] additional check for data consistency.
-#define SZX_DEBUG_TEST  1
-#endif // SZX_DEBUG
+// [off] activate test codes like additional check for data consistency..
+#define SZX_DEBUG  _CC_MS_VC
 #pragma endregion DebugHelper
 
 
@@ -88,4 +104,4 @@
 #pragma endregion SolverBehavior
 
 
-#endif // AGAIN_SZX_MP_SOLVER_CONFIG_H
+#endif // SMART_SZX_P_CENTER_CONFIG_H
